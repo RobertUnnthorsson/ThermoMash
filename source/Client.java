4680db71d005c2964c1d68bfb62d8c8b34d34801
Robert Unnthorsson <09robert.u@gmail.com>
2013-04-27 12:04:06 +0300

    Update Client.java
    Code changed according to naming conventions.

--- source/Client.java
+++ source/Client.java
@@ -1,13 +1,13 @@
 /****************************************************************************************
  * Title      :    Temperature Client Program
  * Developers :  	Bogdan Ioan Sorlea - s121075@student.dtu.dk, 
- * 					Robert Unnthorsson-s121049@student.dtu.dk
+ * 					      Robert Unnthorsson-s121049@student.dtu.dk
  * Purpose    :  	
  * Revision   : 	1.0    
  * Description:	
  ****************************************************************************************/
+import tcp.client.comm.HandleClientTCP;	// Import the client communication handler
 
-import tcp.client.comm.Handle_Client_TCP;	// Import the client communication handler
 
 public class Client 
 {
@@ -16,7 +16,7 @@ public class Client
    // Variable declarations	  
    String host = "Robson_W700";  // Define a host server
    int port = 5050;             // Define a port
-   Handle_Client_TCP client = new Handle_Client_TCP();
+   HandleClientTCP client = new HandleClientTCP();
 
    // Connect to the server
    client.connectToServer(host, port);
